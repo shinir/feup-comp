0b6b33d731bec1929f99f26a8bf787f764ee0c2c
AfonsoAbreu1922 <72894396+AfonsoAbreu1922@users.noreply.github.com>
2023-03-19 23:07:47 +0000

    if test

--- src/main/pt/up/fe/comp2023/Launcher.java
+++ src/main/pt/up/fe/comp2023/Launcher.java
@@ -40,6 +40,8 @@ public class Launcher {
         // Parse stage
         JmmParserResult parserResult = parser.parse(code, config);
 
+        System.out.println(parserResult.getRootNode().toTree());
+
         // Check if there are parsing errors
         TestUtils.noErrors(parserResult.getReports());
 
