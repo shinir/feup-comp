6e791a4953e55c03034976f8fb5081debb9fd6f0
AfonsoAbreu1922 <72894396+AfonsoAbreu1922@users.noreply.github.com>
2023-04-20 19:21:01 +0100

    some visits made

--- src/main/pt/up/fe/comp2023/ollir/JmmOptimize.java
+++ src/main/pt/up/fe/comp2023/ollir/JmmOptimize.java
@@ -3,6 +3,7 @@ package pt.up.fe.comp2023.ollir;
 import pt.up.fe.comp.jmm.ollir.JmmOptimization;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
+import pt.up.fe.comp2023.MySymbolTable;
 
 import java.util.Collections;
 
--- src/main/pt/up/fe/comp2023/ollir/OllirGenerator.java
+++ src/main/pt/up/fe/comp2023/ollir/OllirGenerator.java
@@ -5,13 +5,15 @@ import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
 import pt.up.fe.comp.jmm.analysis.table.Type;
 import pt.up.fe.comp.jmm.ast.AJmmVisitor;
 import pt.up.fe.comp.jmm.ast.JmmNode;
+import pt.up.fe.comp.jmm.ast.PreorderJmmVisitor;
+import pt.up.fe.comp.jmm.ollir.OllirUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class OllirGenerator extends AJmmVisitor<OllirType, String> {
+public class OllirGenerator extends PreorderJmmVisitor<String, String> {
     private final StringBuilder ollirCode;
     private final SymbolTable symbolTable;
     private int indentationLevel;
@@ -27,39 +29,82 @@ public class OllirGenerator extends AJmmVisitor<OllirType, String> {
         this.ifThenElseNum = 1;
         this.whileNum = 1;
 
+    }
+    @Override
+    protected void buildVisitor() {
         addVisit("Program", this::programVisit);
         addVisit("ClassDeclaration", this::classDeclVisit);
+        addVisit("VariableDeclaration", this::varDeclVisit);
         addVisit("MethodDeclaration", this::methodDeclVisit);
         addVisit("MainMethodDeclaration", this::methodDeclVisit);
         addVisit("MethodCall", this::methodCallVisit);
-        addVisit("MethodCall", this::methodCallVisit);
+        addVisit("NewObj", this::newObjVisit);
         addVisit("BinOp", this::binOpVisit);
         addVisit("Assignment", this::assignmentVisit);
-        addVisit("MethodArguments", this::methodArgsVisit);
         addVisit("IntegerLiteral", this::intLiteralVisit);
         addVisit("BooleanLiteral", this::booleanVisit);
         addVisit("Id", this::idVisit);
-        addVisit("Index", this::indexVisit);
-        addVisit("Length", this::lengthVisit);
-        addVisit("IfStatement", this::ifStatementVisit);
-        addVisit("WhileStatement", this::whileStatementVisit);
-        addVisit("ScopeStatement", this::scopeStatementVisit);
-        addVisit("This", this::thisVisit);
-        addVisit("Not", this::notVisit);
         addVisit("Return", this::returnVisit);
+        addVisit("ExpressionStatement", this::expressionStmtVisit);
+        setDefaultVisit(this::defaultVisit);
+    }
 
+    private String defaultVisit(JmmNode jmmNode, String s) {
+        return "";
     }
 
     public String getCode() {
         return ollirCode.toString();
     }
 
-    private String programVisit(JmmNode start, OllirType var) {
+    private String programVisit(JmmNode start, String var) {
+        for (String importString : symbolTable.getImports()) {
+            ollirCode.append("import ").append(importString).append(";\n");
+        }
 
+        for (JmmNode child : start.getChildren()) {
+            visit(child);
+        }
+        return "";
     }
 
-    private String classDeclVisitVisit(JmmNode classDecl, OllirType var) {
+    private String classDeclVisit(JmmNode classDecl, String var) {
+        ollirCode.append(symbolTable.getClassName() + '{' + '\n');
+
+        var superClass = symbolTable.getSuper();
+
+        if (superClass != null) {
+            ollirCode.append(" extends ").append(superClass);
+        }
+
+        for (var field : symbolTable.getFields()) {
+            ollirCode.append(".field private" + field.getName() + (field.getType().isArray() ? ".array" : "") +  "." + ollirUtils.getOllirType(field.getType().getName()));
+        }
+
+        ollirCode.append(".construct" + symbolTable.getClassName() + "().V {\n invokespecial(this, \"<init>\").V;\n}");
+
+        for (JmmNode child : classDecl.getChildren()){
+            visit(child);
+        }
+
+        ollirCode.append('}');
+
+        return "";
     }
+
+    private String varDeclVisit(JmmNode varDecl, String var){
+        if(!varDecl.getJmmParent().getKind().equals("Class")){
+            return "";
+        }
+        Type type = ollirUtils.getType(varDecl.getJmmChild(0));
+
+        ollirCode.append(".field private ")
+                .append(ollirUtils.getCode(
+                        new Symbol(type, varDecl.get("value"))
+                )).append(";\n");
+        return "";
+    }
+
 
 
 }
