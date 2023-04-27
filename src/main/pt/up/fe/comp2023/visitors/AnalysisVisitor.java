ae1a3d2fc57db14c7944b0a4530154ff68cbdc1f
shini <raquelmcarneiro16@gmail.com>
2023-04-27 22:22:16 +0100

    Revert "maybe?"
    This reverts commit 5a8046f8457e8c6e41f0f39fbe5f4a9382485283.

--- src/main/pt/up/fe/comp2023/visitors/AnalysisVisitor.java
+++ src/main/pt/up/fe/comp2023/visitors/AnalysisVisitor.java
@@ -7,7 +7,6 @@ import pt.up.fe.comp.jmm.ast.PreorderJmmVisitor;
 import pt.up.fe.comp.jmm.report.Report;
 import pt.up.fe.comp.jmm.report.ReportType;
 import pt.up.fe.comp.jmm.report.Stage;
-import pt.up.fe.comp2023.Method;
 import pt.up.fe.comp2023.MySymbolTable;
 import pt.up.fe.comp2023.utils.AnalysisUtils;
 
@@ -18,191 +17,143 @@ public class AnalysisVisitor extends PreorderJmmVisitor<MySymbolTable, Boolean> 
     private final AnalysisUtils utils = new AnalysisUtils();
     List<Report> reports = new ArrayList<Report>();
     private final List<String> types = new ArrayList<>();
+    private final String defaultMessage = "DEFAULT ERROR MESSAGE";
 
     @Override
     protected void buildVisitor() {
-        addVisit("Program", this::dealWithProgram);
-        addVisit("Import", this::dealWithImports);
+        addVisit("importDeclaration", this::dealWithImports);
         addVisit("Class", this::dealWithClass);
         addVisit("FunctionMethodDeclaration", this::dealWithFunctionMethod);
-        addVisit("MainMethodDeclaration", this::dealWithMainMethod);
+        addVisit("MainMethodDeclaration", this::dealWithFunctionMethod);
         this.setDefaultVisit(this::myVisitAllChildren);
     }
 
-    private Boolean dealWithProgram(JmmNode jmmNode, MySymbolTable symbolTable) {
-        return true;
-    }
-
     private Boolean myVisitAllChildren(JmmNode jmmNode, MySymbolTable symbolTable) {
+
+        for (JmmNode child : jmmNode.getChildren()){
+            visit(child, symbolTable);
+        }
         return true;
     }
 
     private Boolean dealWithImports(JmmNode jmmNode, MySymbolTable symbolTable) {
-        System.out.println("here");
-        StringBuilder imported = new StringBuilder();
-        String importString = jmmNode.get("importName");
-        for (String path : importString.substring(1 , importString.length() -1 ).replaceAll("\\s+", "").split(",")){
-            imported.append(path);
-            imported.append(".");
+        StringBuilder path = new StringBuilder();
+
+        for (JmmNode node : jmmNode.getChildren()) {
+            path.append(node.get("name"));
+            path.append('.');
         }
+        path.deleteCharAt(path.length() -1);
 
-        imported.deleteCharAt(imported.length()-1);
-        String importedPath = imported.toString();
-
-        if (symbolTable.getImports().contains(importedPath)){
-            String last = importedPath.substring(importedPath.lastIndexOf(".") +1);
+        if (!symbolTable.getImports().contains(path.toString())){
+            String last = path.toString().substring(path.toString().lastIndexOf('.')+1);
             Optional<String> match = symbolTable
                     .getImports()
                     .stream()
-                    .filter(s -> s.substring(s.lastIndexOf('.') + 1).equals(last))
+                    .filter(s -> s.substring(s.lastIndexOf('.')+1).equals(last))
                     .findAny();
-            if (match.isPresent()) {
-                Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Already imported");
+
+            if (match.isPresent()){
+                Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, 0, 0, defaultMessage);
                 reports.add(newReport);
                 return true;
             }
+            symbolTable.addImports(path.toString());
         }
-        symbolTable.addImports(importedPath);//must
-
 
         return true;
     }
 
     private Boolean dealWithClass(JmmNode jmmNode, MySymbolTable symbolTable) {
-        //System.out.println();
-        //must
         symbolTable.addClassName(jmmNode.get("name"));
-        if(jmmNode.hasAttribute("superClass")) {
-            symbolTable.addSupers(jmmNode.get("superClass"));
-        }
-        for (var node : jmmNode.getChildren()){
-            if (node.getKind().equals("VarDeclaration")) {
-                Type type = utils.getType(node.getJmmChild(0), symbolTable);
-                Symbol symbol = new Symbol(type, node.get("name"));
 
-                if (symbolTable.getFields().stream().anyMatch(s -> s.getName().equals(symbol.getName()))) {
-                    Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Variable already defined ");
+
+        if (jmmNode.getAttributes().contains("superClass"))
+            symbolTable.addSupers(jmmNode.get("superClass"));
+
+        for (JmmNode node : jmmNode.getChildren()){
+
+            if (node.getKind().equals("varDeclaration")) {
+                Symbol symbol = utils.getSymbol(node);
+
+                // checks whether a symbol with the same name
+                // as the symbol object already exists in the
+                // symbol table's list of fields.
+                if (symbolTable.getFields().stream().anyMatch(s -> s.getName().equals(symbol.getName()))){
+                    Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, 0, 0, defaultMessage);
                     reports.add(newReport);
                 }
                 else {
                     symbolTable.addFields(symbol);
                 }
             }
-            /*
             else {
                 visit(node, symbolTable);
-            }*/
-
+            }
         }
-        //
         return true;
     }
 
     private Boolean dealWithFunctionMethod(JmmNode jmmNode, MySymbolTable symbolTable) {
-        // must
-        String functionName = jmmNode.get("funcName");;
-        Type returnType = utils.getType(jmmNode.getJmmChild(0), symbolTable);
+        String functionName = "";
+        Type returnType;
+
+        if (jmmNode.getAttributes().contains("funcName")){
+            System.out.println(jmmNode);
+            functionName = jmmNode.get("funcName");
+            returnType = utils.getType(jmmNode.getJmmChild(0));
+        }
+        else {
+            functionName = jmmNode.get("name");
+            returnType = new Type("void", false);
+        }
+
         List<Symbol> parameters = new ArrayList<>();
         List<Symbol> variables = new ArrayList<>();
 
-        for(JmmNode node : jmmNode.getChildren()) {
-            if(node.getKind().equals("VarDeclaration")) {
-                Symbol symbol = utils.getSymbol(node, symbolTable);
-                if (Stream.concat(variables.stream(), parameters.stream()).anyMatch(s -> s.getName().equals(symbol.getName()))){
-                    Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Already defined ");
+
+        for (JmmNode node : jmmNode.getChildren()) {
+
+            if (node.getKind().equals("parameter")) {
+                Symbol param = new Symbol(utils.getType(node.getJmmChild(0)), node.getJmmChild(0).get("name"));
+
+                if (parameters.stream().anyMatch(s -> s.getName().equals(param.getName()))) {
+                    Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, 0, 0,defaultMessage);
                     reports.add(newReport);
+                } else {
+                    parameters.add(param);
                 }
-                else {
+            }
+
+            if (node.getKind().equals("varDeclaration")) {
+                Symbol symbol = utils.getSymbol(node);
+                if (Stream.concat(variables.stream(), parameters.stream()).anyMatch(s -> s.getName().equals(symbol.getName()))) {
+
+                    Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, 0, 0, defaultMessage);
+                    reports.add(newReport);
+                } else {
                     variables.add(symbol);
                 }
             }
 
-            if(node.getKind().equals("Parameter")) {
-                Symbol param = new Symbol(utils.getType(node.getJmmChild(0), symbolTable), node.get("name"));
-                if (parameters.stream().anyMatch(s -> s.getName().equals(param.getName()))) {
-                    Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Already defined ");
-                    reports.add(newReport);
-                }
-                else {
-                    parameters.add(param);
-                }
-            }
         }
 
-        StringBuilder signature = new StringBuilder();
-        signature.append(functionName);
-        for (Symbol param : parameters){
-            signature.append("#");
-            signature.append(param.getType().print());
+        StringBuilder method = new StringBuilder();
+        method.append(functionName);
+        for (Symbol parameter: parameters) {
+            method.append("#");
+            method.append(parameter.getType().print());
         }
-        //String stringSignature = signature.toString();
 
-        if (symbolTable.getMethods().contains(signature.toString())){
-            Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Already defined method");
+        if (symbolTable.getMethods().contains(method.toString())){
+            Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, 0, 0, defaultMessage);
             reports.add(newReport);
         }
         else {
-            symbolTable.addMethods(signature.toString(), parameters, variables, returnType);
-        }
-    /*
-        // CHECK RETURN TYPES
-        if(returnType != null && parameters.isEmpty() && variables.isEmpty()) {
-            if(symbolTable.getImports().isEmpty()) {
-                Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Class not imported.");
-                reports.add(newReport);
-            }
-            // what if the returnType is a from an imported class?
-            Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Variable not declared.");
-            reports.add(newReport);
-        }
-*/
-        /*
-        // CHECK ASSIGNMENTS
-        List<String> types = new ArrayList<>();
-        Boolean check = true;
-        System.out.println(jmmNode.getChildren());
-        for(JmmNode child : jmmNode.getChildren()) {
-            if(child.getKind().equals("VarDeclaration")) {
-                System.out.println(child.getJmmChild(0).getKind());
-                types.add(child.getJmmChild(0).getKind());
-            }
-            if(child.getKind().equals("Assignment")) {
-                System.out.println(child.getJmmChild(0).getKind());
-                for(String type : types) {
-                    if(child.getJmmChild(0).getKind() == type) check = false;
-                }
-            }
+            symbolTable.addMethods(functionName, parameters, variables, returnType);
         }
 
-        if(check) {
-            Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Bad assignment.");
-            reports.add(newReport);
-        }
-        */
-
-        //symbolTable.addMethods(functionName, parameters, variables, returnType);
-
-        jmmNode.put("signature", signature.toString());
-        return true;
-    }
-
-    private Boolean dealWithMainMethod(JmmNode jmmNode, MySymbolTable symbolTable) {
-        List<Symbol> parameters = new ArrayList<>();
-        List<Symbol> variables = new ArrayList<>();
-
-        for(JmmNode node : jmmNode.getChildren()) {
-            if(node.getKind().equals("VarDeclaration")) {
-                Symbol symbol = utils.getSymbol(node, symbolTable);
-                variables.add(symbol);
-            }
-
-            if(node.getKind().equals("Parameter")) {
-                Symbol param = new Symbol(utils.getType(node.getJmmChild(0), symbolTable), node.get("name"));
-                parameters.add(param);
-            }
-        }
-
-        symbolTable.addMethods("main", parameters, variables, new Type("void", false));
+        jmmNode.put("signature", method.toString());
         return true;
     }
 
--- src/main/pt/up/fe/comp2023/visitors/VariableVisitor.java
+++ src/main/pt/up/fe/comp2023/visitors/VariableVisitor.java
@@ -1,6 +1,5 @@
 package pt.up.fe.comp2023.visitors;
 
-import pt.up.fe.comp.jmm.analysis.table.Symbol;
 import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
 import pt.up.fe.comp.jmm.ast.PreorderJmmVisitor;
 import pt.up.fe.comp.jmm.report.ReportType;
@@ -12,232 +11,52 @@ import pt.up.fe.comp.jmm.ast.AJmmVisitor;
 import pt.up.fe.comp.jmm.ast.JmmNode;
 import pt.up.fe.comp.jmm.report.Report;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Stream;
 
 public class VariableVisitor extends PreorderJmmVisitor<MySymbolTable, Boolean> {
     private final AnalysisUtils utils = new AnalysisUtils();
-    private final List<String> types = new ArrayList<>();
-    List<Report> reports = new ArrayList<Report>();
 
     static final List<String> PRIMITIVES = Arrays.asList("int", "void", "boolean");
     static final List<String> ARITHMETIC_OP = Arrays.asList("+", "-", "*", "/");
     static final List<String> COMPARISON_OP = List.of("<");
     static final List<String> LOGICAL_OP = List.of("&&");
+    private final List<String> types = new ArrayList<>();
+    List<Report> reports = new ArrayList<Report>();
 
     @Override
     protected void buildVisitor() {
-        addVisit("Class", this::dealWithClass);
         addVisit("VarDeclaration", this::dealWithVarDeclaration);
-        addVisit("CallFunction", this::dealWithCall);
         addVisit("BinaryOp", this::dealWithBinaryOp);
-        addVisit("Variable", this::dealWithVariable);
         addVisit("ComparisonOp", this::dealWithComparisonOp);
-        addVisit("Assignment", this::dealWithAssignment);
-        addVisit("ArrayAccess", this::dealWithArrayAccess);
         addVisit("This", this::dealWithThis);
         this.setDefaultVisit(this::myVisitAllChildren);
-    }
-
-    private Boolean dealWithCall(JmmNode jmmNode, MySymbolTable symbolTable) {
-        System.out.println(jmmNode);
-
-
-        for (String s : symbolTable.getImports()){
-            if (s.substring(s.lastIndexOf(".") + 1).equals(jmmNode.getJmmChild(0).get("name"))) return true;
-        }
-        for (Symbol f : symbolTable.getFields()){
-            if (f.getName().equals(jmmNode.getJmmChild(0).get("name"))) return true;
-        }
-
-        String method;
-        JmmNode parent = jmmNode.getJmmParent();
-        while (!parent.getKind().equals("MethodDeclaration")){
-            parent = parent.getJmmParent();
-        }
-        parent = parent.getJmmChild(0);
-        method = parent.get("signature");
-
-        for (Symbol s : symbolTable.getParameters(method)){
-            if (s.getName().equals(jmmNode.getJmmChild(0).get("name"))) return true;
-        }
-        for (Symbol s : symbolTable.getLocalVariables(method)){
-            if (s.getName().equals(jmmNode.getJmmChild(0).get("name"))) return true;
-        }
-
-        Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Not imported");
-        reports.add(newReport);
-        return true;
-    }
-
-    private Boolean dealWithVariable(JmmNode jmmNode, MySymbolTable symbolTable) {
-        //if (symbolTable.getImports())
-        //System.out.println(symbolTable.getImports());
-        if (jmmNode.getJmmParent().getKind().equals("CallFunction")) return true;
-
-        if (utils.getVariableType(jmmNode, symbolTable) == null){
-            Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Variable not declared");
-            reports.add(newReport);
-            return true;
-        }
-        return true;
-    }
-
-    private Boolean dealWithBinaryOp(JmmNode jmmNode, MySymbolTable symbolTable) {
-
-        JmmNode left = jmmNode.getJmmChild(0);
-        Type leftType = utils.getType(left, symbolTable);
-        if (leftType == null){
-            Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Cannot find symbol");
-            reports.add(newReport);
-            return true;
-        }
-
-        JmmNode right = jmmNode.getJmmChild(1);
-        Type rightType = utils.getType(right, symbolTable);
-        if (rightType == null){
-            Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Cannot find symbol");
-            reports.add(newReport);
-            return true;
-        }
-
-
-        if (leftType.getName().equals("#UNKNOWN")
-                || rightType.getName().equals("#UNKNOWN")){
-            jmmNode.put("type", "#UNKNOWN");
-            return true;
-        }
-
-        //System.out.println(leftType);
-        //System.out.println(rightType);
-
-        if (!leftType.getName().equals(rightType.getName())){
-            jmmNode.put("type", "#UNKNOWN");
-            Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Types dont match");
-            reports.add(newReport);
-        }
-
-        if (ARITHMETIC_OP.contains(jmmNode.get("op"))
-                || LOGICAL_OP.contains(jmmNode.get("op"))
-                || COMPARISON_OP.contains(jmmNode.get("op"))){
-            if (leftType.isArray()){
-                jmmNode.put("type", "#UNKNOWN");
-                Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Cannot be applied");
-                reports.add(newReport);
-                return true;
-            }
-        }
-
-        if (ARITHMETIC_OP.contains(jmmNode.get("op"))
-                || COMPARISON_OP.contains(jmmNode.get("op"))){
-            if (!leftType.getName().equals("int")) {
-                jmmNode.put("type", "#UNKNOWN");
-                Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Operation cannot be applied");
-                reports.add(newReport);
-                return true;
-            }
-        }
-
-        if (LOGICAL_OP.contains(jmmNode.get("op"))){
-            if (!leftType.getName().equals("boolean")){
-                jmmNode.put("type", "#UNKNOWN");
-                Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Types dont match");
-                reports.add(newReport);
-                return true;
-            }
-        }
-
-        if (!PRIMITIVES.contains(leftType.getName())) {
-            jmmNode.put("type", "#UNKNOWN");
-            Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Types dont match");
-            reports.add(newReport);
-            return true;
-        }
-
-        if (COMPARISON_OP.contains(jmmNode.get("op"))) {
-            jmmNode.put("type", new Type("boolean", false).getName());
-        } else {
-            jmmNode.put("type", leftType.getName());
-        }
-        return true;
-    }
-
-    private Boolean dealWithClass(JmmNode jmmNode, MySymbolTable symbolTable) {
-
-        if (!jmmNode.getAttributes().contains("extends")){
-            return true;
-        }
-
-        String superName = jmmNode.get("superClass");
-        if (jmmNode.get("name").equals(superName)){
-            Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Class cant extend herself");
-            reports.add(newReport);
-            return true;
-        }
-
-        boolean foundImport = symbolTable
-                .getImports()
-                .stream()
-                .anyMatch(imp -> imp.substring(imp.lastIndexOf(".") + 1).equals(superName));
-
-        if (foundImport) {
-            return true;
-        }
-
-        Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Cant found that class on any import");
-        reports.add(newReport);
-
-        return true;
     }
 
     private Boolean myVisitAllChildren(JmmNode jmmNode, MySymbolTable symbolTable) {
         return true;
     }
 
+    private boolean dealWithBinaryOp(JmmNode jmmNode, MySymbolTable symbolTable) {
+        Type lhsType = utils.getType(jmmNode.getJmmChild(0));
+        Type rhsType = utils.getType(jmmNode.getJmmChild(1));
+
+
+        return true;
+    }
     private Boolean dealWithComparisonOp(JmmNode jmmNode, MySymbolTable symbolTable) {
         return true;
     }
 
     private Boolean dealWithThis(JmmNode jmmNode, MySymbolTable symbolTable) {
-        if(jmmNode.getAncestor("methodDeclaration").isPresent()) {
-            jmmNode.put("type", "#UNKNOWN");
-            Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Non static cannot be referenced from a static context.");
-            reports.add(newReport);
-        }
         return true;
     }
 
     private Boolean dealWithVarDeclaration(JmmNode jmmNode, MySymbolTable symbolTable) {
-
-        return true;
-    }
-
-    private Boolean dealWithAssignment(JmmNode jmmNode, MySymbolTable symbolTable) {
-        //for(jmmNode.getHierarchy())
-
-        //System.out.println(jmmNode.getJmmParent().getChildren());
-        return true;
-    }
-
-    private Boolean dealWithArrayAccess(JmmNode jmmNode, MySymbolTable symbolTable) {
-        //System.out.println(jmmNode);
-        Type integer = new Type("int", false);
-        Type array = new Type("int", true);
-
-        if(!utils.getType(jmmNode.getJmmChild(1),symbolTable).equals(integer)) {
-            Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Array access not integer.");
-            reports.add(newReport);
-        }
-
-        if (!utils.getVariableType(jmmNode.getJmmChild(0), symbolTable).equals(array)){
-            Report newReport = new Report(ReportType.ERROR, Stage.SEMANTIC, Integer.parseInt(jmmNode.get("lineStart")), Integer.parseInt(jmmNode.get("colStart")), "Not an array.");
-            reports.add(newReport);
-        }
-
+        Type type = utils.getType(jmmNode.getJmmChild(0));
+        if(!types.contains(type.getName())) reports.add(new Report(ReportType.ERROR, Stage.SEMANTIC, 0, "Class unknown/not instanced."));
         return true;
     }
 
